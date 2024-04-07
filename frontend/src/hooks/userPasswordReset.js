a5ddfa94568cd838f15137aea7fd75524f9cf0ef
Luca <luca.cassier@hotmail.be>
2024-04-07 12:19:19 +0200

    passwordreset hook and page

---
+++ frontend/src/hooks/userPasswordReset.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import { useVerificationContext } from "./useVerificationContext";
+
+
+export const usePasswordReset = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+  const { verification } = useVerificationContext();
+
+
+  const passwordReset = async (password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch("/api/user/password-reset", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email: verification.user.email , password }),
+    });
+    
+    const json = await response.json();
+    console.log(json)
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+    if (response.ok) {
+
+      // save user to local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      // update the auth context
+      dispatch({ type: "LOGIN", payload: json });
+      setIsLoading(false);
+    }
+  };
+  return { passwordReset, isLoading, error };
+};
