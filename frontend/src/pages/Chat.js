6f0503f5f3a52d6132f552854968d5b704537d8f
lccua <111664939+lccua@users.noreply.github.com>
2024-04-27 11:11:00 +0200

    made it so that the switch automatically changes to the opposite state when a message is send

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -1,16 +1,15 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './Chat.css'; // Import CSS file for styling
-import './ToggleSwitch.css';
+import React, { useState, useRef, useEffect } from "react";
+import "./Chat.css"; // Import CSS file for styling
+import "./ToggleSwitch.css";
 
 const DummyMessages = [
-  { id: 1, sender: 'future-me', message: 'Hey there!' },
-  { id: 2, sender: 'present-me', message: 'Hi John, how are you?' },
- 
+  { id: 1, sender: "future-me", message: "Hey there!" },
+  { id: 2, sender: "present-me", message: "Hi John, how are you?" },
 ];
 
 const Chat = () => {
   const [messages, setMessages] = useState(DummyMessages);
-  const [newMessage, setNewMessage] = useState('');
+  const [newMessage, setNewMessage] = useState("");
   const chatRef = useRef();
   const [isSwitchOn, setIsSwitchOn] = useState(false);
 
@@ -24,20 +23,23 @@ const Chat = () => {
   };
 
   const handleSendMessage = () => {
-    if (newMessage.trim() === '') return; // Don't send empty messages
-    const sender = isSwitchOn ? 'future-me' : 'present-me'; // Determine the sender based on the toggle state
+    if (newMessage.trim() === "") return; // Don't send empty messages
+    const sender = isSwitchOn ? "future-me" : "present-me"; // Determine the sender based on the toggle state
     const newMsg = {
       id: messages.length + 1,
       sender: sender, // Assign the determined sender
-      message: newMessage
+      message: newMessage,
     };
     setMessages([...messages, newMsg]);
-    setNewMessage('');
+    setNewMessage("");
+    setIsSwitchOn(!isSwitchOn);
+
+
+
   };
-  
 
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter' && newMessage.trim() !== '') {
+    if (e.key === "Enter" && newMessage.trim() !== "") {
       handleSendMessage();
     }
   };
@@ -50,23 +52,27 @@ const Chat = () => {
 
   return (
     <div className="chat-container">
-      <div className='chat-header'>
-        <h2 className='chat-header-title'>Chat header</h2>
+      <div className="chat-header">
+        <h2 className="chat-header-title">Chat header</h2>
       </div>
       <div className="chat" ref={chatRef}>
-      {messages.map((msg) => (
-  <div key={msg.id} className={`chat-msg ${msg.sender === 'future-me' ? 'future-me' : ''}`}>
-    <span>{msg.message}</span>
-  </div>
-))}
-
+        {messages.map((msg) => (
+          <div
+            key={msg.id}
+            className={`chat-msg ${
+              msg.sender === "future-me" ? "future-me" : ""
+            }`}
+          >
+            <span>{msg.message}</span>
+          </div>
+        ))}
       </div>
       <div className="input-container">
         <label className="switch">
           <input type="checkbox" onChange={toggleSwitch} checked={isSwitchOn} />
           <span className="slider"></span>
         </label>
-        <div className={`input-field ${isSwitchOn ? '' : 'orange-focus'}`}>
+        <div className={`input-field ${isSwitchOn ? "" : "orange-focus"}`}>
           <input
             type="text"
             value={newMessage}
@@ -74,7 +80,13 @@ const Chat = () => {
             onKeyPress={handleKeyPress}
             placeholder="Type your message..."
           />
-          <button onClick={handleSendMessage} className={newMessage.trim() === '' ? 'button-disabled' : ''} disabled={!newMessage.trim()}>Send</button>
+          <button
+            onClick={handleSendMessage}
+            className={newMessage.trim() === "" ? "button-disabled" : ""}
+            disabled={!newMessage.trim()}
+          >
+            Send
+          </button>
         </div>
       </div>
     </div>
